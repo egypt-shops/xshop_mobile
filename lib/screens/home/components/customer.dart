b4244c17e387a6d9b05107701f11e2bee8b0a448
ramysaafan <crow7@protonmail.com>
2020-12-09 02:57:33 +0200

    user_search screen (#51)

--- lib/screens/home/components/customer.dart
+++ lib/screens/home/components/customer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:xshop_mobile/screens/login/login.dart';
 import 'package:xshop_mobile/screens/data_entry/main_of_data_entry.dart';
+import '../../user_search/user_search.dart';
 
 class Customer extends StatefulWidget {
   @override
@@ -83,6 +84,33 @@ class _CustomerState extends State<Customer> {
                   children: [
                 Text('Customer',
                     style: TextStyle(fontSize: 30, color: Colors.blue)),
+                Container(
+                  child: Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (c) => SearchProduct()),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white30,
+                            borderRadius: BorderRadius.all(Radius.circular(8))),
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        margin: EdgeInsets.only(right: 20),
+                        child: Text(
+                          'Search Product',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue),
+                        ),
+                      ),
+                    ),
+                  ),
+                  color: Colors.blue[50],
+                ),
               ])),
           drawer: Drawer(
             child: ListView(
