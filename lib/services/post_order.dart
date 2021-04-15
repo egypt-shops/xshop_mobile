c39355fb7751523a7d7c02b8aba4dbd315b8b318
xc0rv0 <crow7@protonmail.com>
2021-04-15 14:06:07 +0200

    post order

--- lib/services/post_order.dart
+++ lib/services/post_order.dart
@@ -3,25 +3,27 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:xshop_mobile/models/order.dart';
-
-Future<Order> createOrder(
-    String id, String user, String shop, String paid) async {
-  final response = await http.post(
-    Uri.https('https://dev-egshops.herokuapp.com/api/orders/', 'albums'),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{
-      'id': id,
-      'user': user,
-      'shop': shop,
-      'paid': paid,
-    }),
-  );
-
-  if (response.statusCode == 201) {
-    return Order.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to create album.');
+class PostOrder {
+  Future<Order> createOrder( String user, String shop,
+      bool paid) async {
+    final response = await http.post(
+      ('https://dev-egshops.herokuapp.com/api/orders/'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'user': user,
+        'shop': shop,
+        'paid': paid,
+      }),
+    );
+    print(response.body+"\n "+response.statusCode.toString());
+    if (response.statusCode == 200) {
+      print('order over there amigo');
+      return Order.fromJson(jsonDecode(response.body));
+    } else {
+      print('see nothing');
+      throw Exception('Failed to create Order.');
+    }
   }
 }
