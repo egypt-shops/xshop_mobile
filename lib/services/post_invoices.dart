bfe1da9d26f2646a9896f51bec2a600a6f4a820f
Ahmed Mohamed Abdallah <benten12536.com>
2021-04-26 18:23:43 +0200

    Post_invoices

---
+++ lib/services/post_invoices.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:xshop_mobile/models/invoice.dart';
+import 'package:http/http.dart' as http;
+
+Future<Invoice> createInvoices(String user, String order) async {
+  final response = await http.post(
+      'https://dev-egshops.herokuapp.com/api/invoices/',
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'user': int.parse(user),
+        'order': int.parse(order)
+      }));
+
+  if (response.statusCode == 200) {
+    return Invoice.fromJson(jsonDecode(response.body));
+  } else {
+    throw ('failed to load');
+  }
+}
